dfbbda60c6df202f0c6d286b8c30c386c3315253
Sake Wolf <nocturne.wolf@hotmail.com>
2016-02-20 01:30:49 -0800

    Cleaning app code and allowing the use of static files

--- app/app.js
+++ app/app.js
@@ -1,11 +1,6 @@
-(function() {
-	var express = require('express');
-	var app = express();
+var express = require('express');
+var app = express();
 
-	app.get('/', function(request, response) {
-		response.send('Hi!');
-	});
-	
-	console.log("Initializing server on port 3000...");
-	app.listen(3000);
-})();
+app.use(express.static('public'));
+
+module.exports = app;
