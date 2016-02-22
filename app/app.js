efad6ae475f93949afa88973852b225da24dad05
SakeWolf <nocturne.wolf@hotmail.com>
2016-02-22 14:04:46 -0800

    Updated routes to reflect change of blog name

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('view engine', 'jade');
 
 app.get('/', function (request, response) {
-	response.render('index', {title: 'StarpowerFM', message: 'hOI! i\'m hTML!'});
+	response.render('index', {title: 'Wolf Chords', message: 'hOI! i\'m hTML!'});
 });
 
 var reviews = require('./routes/reviews');
--- app/routes/reviews.js
+++ app/routes/reviews.js
@@ -6,13 +6,13 @@ var router = express.Router();
 
 router.route('/')
 .get(function(request, response) {
-	response.render('review', {title: "StarpowerFM", review: "Content!"});
+	response.render('review', {title: "Wolf Chords", review: "Content!"});
 });
 
 router.route('/:album')
 .get(function(request, response) {
 	var id = request.params.album;
-	response.render('review', {title: "StarpowerFM", review: "Something!"});
+	response.render('review', {title: "Wolf Chords", review: "Something!"});
 });
 
 module.exports = router;
