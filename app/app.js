8a7e2539b92b89bc73010b598fc4b6d4eab343fe
SakeWolf <nocturne.wolf@hotmail.com>
2016-02-24 13:47:18 -0800

    Integrating passport for local authentication

--- app/app.js
+++ app/app.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require("passport-local").Strategy;
+var reviews = require('./routes/reviews');
+var fetch = require('./routes/fetch');
 
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		return done(null, true);
+	}
+));
+
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+	done(null, user);
+});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	secret: 'guitar dog',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'jade');
+app.use('/reviews', reviews);
+app.use('/fetch', fetch);
+
 
 app.get('/', function (request, response) {
 	response.render('index', {title: 'Wolf Chords', message: 'hOI! i\'m hTML!'});
 });
 
-var reviews = require('./routes/reviews');
-app.use('/reviews', reviews);
-
-var fetch = require('./routes/fetch');
-app.use('/fetch', fetch);
 
 module.exports = app;
--- app/routes/fetch.js
+++ app/routes/fetch.js
@@ -1,12 +1,20 @@
 var express = require('express');
 
-var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var router = express.Router();
 
 router.route('/')
 .get(function (request, response) {
 	response.render('login', {});
+})
+.post(
+	passport.authenticate('local', {successRedirect: '/fetch/hub', failureRedirect: '/fetch'})
+);
+
+router.route('/hub')
+.get(function (request, response) {
+	response.render('index', {title: "Fetch Hub", message: "For admin eyes only!"});
 });
 
 module.exports = router;
